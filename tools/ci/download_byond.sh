43483e7be5909a4a3c495138020d6a0657a6318e
Cupa <5896309+Cupax3@users.noreply.github.com>
2026-01-05 13:11:06 +0100

    "Temporary" CI fix

--- tools/ci/download_byond.sh
+++ tools/ci/download_byond.sh
@@ -2,4 +2,4 @@
 set -e
 source dependencies.sh
 echo "Downloading BYOND version $BYOND_MAJOR.$BYOND_MINOR"
-curl --user-agent "lc13-ci/1.0 (Linux; curl/8; +https://github.com/Lobotomy-Corporation-13/lc13)" "$MIRROR_URL/${BYOND_MAJOR}/${BYOND_MAJOR}.${BYOND_MINOR}_byond.zip" -o C:/byond.zip
+curl --user-agent "lc13-ci/1.0 (Linux; curl/8; +https://github.com/Lobotomy-Corporation-13/lc13)" "https://lc13.net/temp-ci/${BYOND_MAJOR}/${BYOND_MAJOR}.${BYOND_MINOR}_byond.zip" -o C:/byond.zip
--- tools/ci/install_byond.sh
+++ tools/ci/install_byond.sh
@@ -11,7 +11,7 @@ else
   rm -rf "$HOME/BYOND"
   mkdir -p "$HOME/BYOND"
   cd "$HOME/BYOND"
-  curl --user-agent "lc13-ci/1.0 (Linux; curl/8; +https://github.com/Lobotomy-Corporation-13/lc13)" "$MIRROR_URL/${BYOND_MAJOR}/${BYOND_MAJOR}.${BYOND_MINOR}_byond_linux.zip" -o byond.zip
+  curl --user-agent "lc13-ci/1.0 (Linux; curl/8; +https://github.com/Lobotomy-Corporation-13/lc13)" "https://lc13.net/temp-ci/${BYOND_MAJOR}/${BYOND_MAJOR}.${BYOND_MINOR}_byond_linux.zip" -o byond.zip
   unzip byond.zip
   rm byond.zip
   cd byond
